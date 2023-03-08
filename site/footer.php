0639bc470a8a75f43f969db203dc64cf7b2032fb
Bart Groothuysen <bart@groothuysen.com>
2023-03-08 09:05:55 +0100

    website bestanden toevoegen

---
+++ site/footer.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<footer>
+    <div class="container">
+        <p> <a href="https://www.google.com/maps/place/Kenaupark+22,+2011+MT+Haarlem/@52.3858699,4.6311919,17z/data=!3m1!4b1!4m5!3m4!1s0x47c5ef6d2fd14c8f:0x6917283c817b37b4!8m2!3d52.3858699!4d4.6333806?hl=nl&shorturl=1">
+          Kenupark 22</a>    | 2011 Mt Haarlem | 06-10770444 | <a href="https://www.google.com/intl/nl/gmail/about/">info@sabijnarts.nl</a> </p>
+    </div>
+  </footer>
+</body>
+</html>
---
+++ site/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header>  
+    
+    <div class="container">
+     <img class="image-4" src="Images/header-image.png" alt="">
+      <nav>
+        <ul>
+          <li> <a href="#">Home</a></li>
+          <li> <a href="#">Sessies</a></li>
+          <li> <a href="#">Retraites</a></li>
+          <li> <a href="#">Online</a></li>
+          <li> <a href="#">Over Sabijn</a></li>
+          <li> <a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</body>
+</html>
