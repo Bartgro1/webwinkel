bc65e14da92ed5002b7b1f7ac05d0c7595ad2a26
Bart Groothuysen <bart@groothuysen.com>
2023-03-09 15:29:46 +0100

    flex container aangepast met php

--- site/product-index.php
+++ site/product-index.php
@@ -1,9 +1,9 @@
 <?php
-   require 'database.php';
+require 'database.php';
 
-   $sql = "SELECT * FROM producten";
-   $result  = mysqli_query($conn,$sql);
-   $producten = mysqli_fetch_all($result,MYSQLI_ASSOC);
+$sql = "SELECT * FROM producten";
+$result  = mysqli_query($conn, $sql);
+$producten = mysqli_fetch_all($result, MYSQLI_ASSOC);
 ?>
 
 <!DOCTYPE html>
@@ -34,80 +34,46 @@
 
       <div class="featured-div">
         <div class="flex-container">
-            <?php foreach ($producten as $product): ?>
-          <div class="flex-box">
-            <h3>Management Intuïtie Dagen</h3>
-            <p class="flex-tekst"> Praktische tools voor zelfstandig ondernemers, managers en leidinggevenden voor het
-              toepassen van de kracht van hun intuïtie in werk. Essentieel voor effectief en inspirerend leiderschap.
+          <?php foreach ($producten as $product) : ?>
+            <div class="flex-box">
+              <h3> <?php echo $product["naam"] ?></h3>
+              <p> <?php echo $product["omschrijving"] ?> </p>
+            </div>
+          <?php endforeach ?>
+        </div>
+      </div>
+      <!--
+      <div class="blogs-outer">
+
+        <div class="featured-content">
+          <div class="item-1">
+            <img class="image-2" src="Images/Afbeelding2.jpg" alt="">
+            <h3 class="featured-hoofdtekst">
+              Highly sensitive and feeling overwhelmed? Connect with your values and listen within
+            </h3>
+            <p class="featured-tekst">
+              Do you try to keep up with the pace of life you see around you, working too hard, feeling out of sync
+              with your body & soul?
             </p>
           </div>
-
-          <div class="flex-box">
-            <h3>Intuïtiedag</h3>
-            <p class="flex-tekst"> Zondag 27 februari in Hof 20 Haarlem. Krachtig leven vanuit je intuïtie. Een heerlijke
-              dag op een fijne plek waarin je je onderdompelt in je eigen wijsheid en het contact met je intuïtie.
-            </p>
-          </div>
-
-          <div class="flex-box">
-            <h3>HSP Retraites 2022 en 2023 </h3>
-            <p class="flex-tekst">HSP BASIS: Je hoogsensitiviteit begrijpen. Van Kwetsbaarheid naar Kracht. HSP
-              VERDIEPING: HSP Heling, Lifestyle en Soul Alignment
-            </p>
-          </div>
-
-          <div class="flex-box">
-            <h3>Individuele coaching</h3>
-            <p class="flex-tekst"> 
-            </p>
-          </div>
-
-          <div class="flex-box">
-            <h3>Herstel Burnout</h3>
-            <p class="flex-tekst">
-              De coaching richt zich in 3 fases op energieherstel, goede selfcare, grenzen, verwerking en verandering
-              van patronen, verhelderen van je koers, met reïntegratie of een nieuwe functie/richting.
-            </p>
-          </div>
-
-          <div class="flex-box">
-            <h3>FROM HEAD TO HEART online cursus voor hsp</h3>
-            <p class="flex-tekst">Van struggle naar acceptatie en zelfliefde, van kwetsbaarheid naar kracht. In 10
-              praktische modules in je kracht met je hoogsensitiviteit, in verbinding met je hart & intuïtie. </p>
-             <?php endforeach ?>
+          <div class="item-2">
+            <h3 class="bloglijst-header"> Meer Blogs:</h3>
+            <ul class="bloglijst">
+              <li>Accepting yourself when you are highly sensitive</li>
+              <li>liefhebben. Vertrouw jij op je inner voice?</li>
+              <li> yourself. Do you trust your inner voice?</li>
+              <li> je met je hart = verbind je met je intuïtie</li>
+              <li> connect with your heart = to connect with your intuition</li>
+              <li> intuïtie is je beste vriend- Sabijn Arts</li>
+              <li> je verlangen, verhelder jouw Core Desired Feelings</li>
+              <li>naar je behoeftes. Het klinkt zo eenvoudig…</li>
+              <li> vanuit je hart, hoe doe je dat?</li>
+              <li> schaam jij je voor?</li>
+            </ul>
           </div>
         </div>
-
-        <div class="blogs-outer">
-
-          <div class="featured-content">
-            <div class="item-1">
-              <img class="image-2" src="Images/Afbeelding2.jpg" alt="">
-              <h3 class="featured-hoofdtekst">
-                Highly sensitive and feeling overwhelmed? Connect with your values and listen within
-              </h3>
-              <p class="featured-tekst">
-                Do you try to keep up with the pace of life you see around you, working too hard, feeling out of sync
-                with your body & soul?
-              </p>
-            </div>
-            <div class="item-2">
-              <h3 class="bloglijst-header"> Meer Blogs:</h3>
-              <ul class="bloglijst">
-                <li>Accepting yourself when you are highly sensitive</li>
-                <li>liefhebben. Vertrouw jij op je inner voice?</li>
-                <li> yourself. Do you trust your inner voice?</li>
-                <li> je met je hart = verbind je met je intuïtie</li>
-                <li> connect with your heart = to connect with your intuition</li>
-                <li> intuïtie is je beste vriend- Sabijn Arts</li>
-                <li> je verlangen, verhelder jouw Core Desired Feelings</li>
-                <li>naar je behoeftes. Het klinkt zo eenvoudig…</li>
-                <li> vanuit je hart, hoe doe je dat?</li>
-                <li> schaam jij je voor?</li>
-              </ul>
-            </div>
-          </div>
-        </div>
+      </div>
+     
         <div class="sabijn-outer">
           <img class="image-3" src="Images/Afbeelding3.jpg" alt="">
           <p>
@@ -119,8 +85,9 @@
           </p>
         </div>
       </div>
+      -->
   </main>
- <?php include 'footer.php';?>
+  <?php include 'footer.php'; ?>
 
 </body>
 
